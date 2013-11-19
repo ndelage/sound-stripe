ca192f2cab7442d3902d37eaca135d5b83b41c19
Nate Delage <nate@natedelage.com>
2013-11-19 16:54:06 -0600

    Working display page

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ PhotoSong.prototype.loadInstagramUrl = function(url) {
   this.instagramId = /p\/(\w+)/.exec(url)[1];
 }
 
-PhotoSong.prototype.imagePreviewUrl = function() {
+PhotoSong.prototype.imageUrl = function() {
   return "http://instagr.am/p/" + this.instagramId + "/media/?size=m";
 }
 
@@ -30,7 +30,7 @@ var EchoNest = {
 function InstagramSongWizardView(selector, model) {
   this.$el = $(selector);
   this.model = model;
-  this.songResultTemplate = _.template("<li data-preview-url='<%=preview_url%>'><%=artist_name%> - <%=title%></li>");
+  this.songResultTemplate = _.template("<li data-preview-url='<%=preview_url%>'><label><input type='radio' name='song'><%=artist_name%> - <%=title%><label></li>");
 
   _.bindAll(this, 'updateInstagram', 'searchSong', 'generateThing')
   $("#instagram-url").on('change', this.updateInstagram);
@@ -46,12 +46,12 @@ InstagramSongWizardView.prototype.show = function(e) {
 InstagramSongWizardView.prototype.updateInstagram = function(e) {
   var instagramUrl = this.$el.find("#instagram-url").val();
   this.model.loadInstagramUrl(instagramUrl);
-  this.$el.find("#image-preview").attr('src', this.model.imagePreviewUrl());
+  this.$el.find("#image-preview").attr('src', this.model.imageUrl());
 };
 
 InstagramSongWizardView.prototype.searchSong = function(e) {
   var self = this;
-  EchoNest.search("radiohead - karma police")
+  EchoNest.search(this.$el.find("#song-title").val())
            .done(function(resp) {
              self.updateSearchResults(resp.response.songs);
            });
@@ -75,17 +75,36 @@ InstagramSongWizardView.prototype.updateSearchResults = function(songs) {
 
 InstagramSongWizardView.prototype.generateThing = function(e) {
   e.preventDefault();
-  var songTitle = this.$el.find("#song-title").val();
-};
+  var selectedSong = this.$el.find("input[name=song]:checked");
+  var songUrl = selectedSong.parents("li").data("preview-url");
+  var url = "http://localhost:8000/?songUrl=" + songUrl + "&instagramId=" + this.model.instagramId;
+  this.$el.find("#final-link").html("<a href='" + url + "'>" + url + "</a>");
+}
 
 $(document).ready(function() {
   var photoSong = new PhotoSong();
   var view = new InstagramSongWizardView("#wizard", photoSong);
 
-  if(window.location.pathname.indexOf("instagram-bug") != -1 ) {
+  if(window.location.href.indexOf("instagramId") != -1 ) {
+   var instagramId = getParameterByName("instagramId");
+   var songUrl = getParameterByName("songUrl");
+   var photoSong = new PhotoSong(instagramId, songUrl);
+
+    var template = _.template("<img id='instagram-image' src='<%=imageUrl%>'><audio autoplay=true src='<%=songUrl%>'></audio>");
+    $("#instagram-song").append(template({songUrl: photoSong.songUrl, imageUrl: photoSong.imageUrl()}));
+    $("audio").on("playing", function() {
+      $("#spinner").hide();
+      $("#instagram-image").fadeIn();
+    });
 
   } else {
     view.show();
   }
 });
 
+function getParameterByName(name) {
+  name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
+  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+  results = regex.exec(location.search);
+  return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
